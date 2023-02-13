47e2ebf62f80ca1c4d9acfc170846b462e796085
AnastasiaGandolfi <118977246+AnastasiaGandolfi@users.noreply.github.com>
2023-02-13 11:33:58 +0100

    fix: change bg-color and default text color

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,3 +1,4 @@
+// copy color names on clipboard
 async function copyTextContent(el) {
     let lowerCase = el.textContent.toLowerCase();
     try {
@@ -6,5 +7,53 @@ async function copyTextContent(el) {
     } catch (err) {
         alert('Failed to copy: ', err);
     }
-    
+
 }
+
+const copyButtonLabel = "Copy Code";
+// use a class selector if available
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+    // only add button if browser supports Clipboard API
+    if (navigator.clipboard) {
+        let button = document.createElement("button");
+
+        button.innerText = copyButtonLabel;
+        block.appendChild(button);
+
+        button.addEventListener("click", async () => {
+            await copyCode(block, button);
+        });
+    }
+});
+
+
+
+async function copyCode(block, button) {
+    let code = block.querySelector("code");
+    let text = code.innerText;
+
+    await navigator.clipboard.writeText(text);
+
+    // visual feedback that task is completed
+    button.innerText = "Code Copied";
+
+    setTimeout(() => {
+        button.innerText = copyButtonLabel;
+    }, 700);
+}
+
+// replace < and > with html entities
+function replaceAllCode() {
+    let codeBlocks = document.querySelectorAll("code");
+    codeBlocks.forEach((codeBlock) => {
+        codeBlock.innerHTML = replaceltgt(codeBlock.innerHTML);
+    });
+}
+
+function replaceltgt(str) {
+    return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
+replaceAllCode();
