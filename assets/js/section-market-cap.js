f43a6535c2da5c4eb999ad6c254c75b012d4a2e8
Katilina88 <caterinalograsso@gmail.com>
2023-02-27 12:36:47 +0100

    feat: add section market cap and section number cap.js

---
+++ assets/js/section-market-cap.js
@@ -0,0 +1,22 @@
+window.addEventListener("scroll", function() {
+  numAnimation(190880140, ".numbers-1");
+  numAnimation(371882681, ".numbers-2");
+  numAnimation(737187243, ".numbers-3");
+  });
+  
+function numAnimation(valoreFinale, selettoreNumero) {
+  let numeri = document.querySelectorAll(selettoreNumero);
+  let count = 0;
+  let differenza = valoreFinale - count;
+  let incremento = differenza / 100;
+  let interval = setInterval(function () {
+    count += incremento;
+    for (var i = 0; i < numeri.length; i++) {
+      numeri[i].innerHTML = count.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+    if (count >= valoreFinale) {
+      clearInterval(interval);
+      numeri[0].innerHTML = valoreFinale.toLocaleString();
+    }
+  }, 10);
+}
