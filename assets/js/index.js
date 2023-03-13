83d533fc990eb51b37296779860cf72c253517c3
georgev97 <georgeandrei.varga@gmail.com>
2023-03-13 15:52:46 +0100

    fix: lots of fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -61,30 +61,33 @@ function changeTheme() {
     icons[i].style.filter = theme == "dark" ? "invert(1)" : "invert(0)";
   }
   if (theme === "dark") {
-    document.getElementById(
-      "waves"
-    ).innerHTML = `<svg viewBox="0 0 1660 339" class="sc-4ba21b47-0 IIbzK slide-svg-light" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 167.023C520.5 167.023 267.5 290.522 0 304.5V339H1660V0.5C1358.83 0.5 1104 167.023 804 167.023Z" fill="url(#paint0_linear_light)"></path><defs><linearGradient id="paint0_linear_light" x1="830" y1="84" x2="830" y2="339" gradientUnits="userSpaceOnUse"><stop stop-color="white" stop-opacity="0.48"></stop><stop offset="0.566389" stop-color="white" stop-opacity="0.35"></stop><stop offset="1" stop-color="white"></stop></linearGradient></defs></svg>`;
-    
+    if(document.getElementById('waves')){
+      document.getElementById(
+        "waves"
+      ).innerHTML = `<svg viewBox="0 0 1660 339" class="sc-4ba21b47-0 IIbzK slide-svg-light" width="100%" color="text" xmlns="http://www.w3.org/2000/svg"><path d="M804 167.023C520.5 167.023 267.5 290.522 0 304.5V339H1660V0.5C1358.83 0.5 1104 167.023 804 167.023Z" fill="url(#paint0_linear_light)"></path><defs><linearGradient id="paint0_linear_light" x1="830" y1="84" x2="830" y2="339" gradientUnits="userSpaceOnUse"><stop stop-color="white" stop-opacity="0.48"></stop><stop offset="0.566389" stop-color="white" stop-opacity="0.35"></stop><stop offset="1" stop-color="white"></stop></linearGradient></defs></svg>`;
+    }
     for (let i = 0; i < lightSwitches.length; i++) {
       lightSwitches[i].style = "transform: translateX(-24px);";
       lightSwitches[i].querySelector("img").src = "assets/icons/sun.svg";
     }
   } else {
-    document.getElementById(
-      "waves"
-    ).innerHTML = `<svg viewBox="0 0 1660 339" class="sc-4ba21b47-0 IIbzK slide-svg-dark" width="100%" color="text" class="icon"
-    xmlns="http://www.w3.org/2000/svg">
-    <path d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z" 
-      fill="url(#paint0_linear_dark)"></path>
-    <defs>
-      <linearGradient id="paint0_linear_dark" x1="830" y1="83.5" x2="830" y2="338.5"
-        gradientUnits="userSpaceOnUse">
-        <stop stop-color="#08060B" stop-opacity="0.2"></stop>
-        <stop offset="0.545554" stop-color="#08060B" stop-opacity="0.5"></stop>
-        <stop offset="1" stop-color="#08060B"></stop>
-      </linearGradient>
-    </defs>
-  </svg>`;
+    if(document.getElementById('waves')){
+      document.getElementById(
+        "waves"
+      ).innerHTML = `<svg viewBox="0 0 1660 339" class="sc-4ba21b47-0 IIbzK slide-svg-dark" width="100%" color="text" class="icon"
+      xmlns="http://www.w3.org/2000/svg">
+      <path d="M804 166.523C520.5 166.523 267.5 290.022 0 304V338.5H1660V0C1358.83 0 1104 166.523 804 166.523Z" 
+        fill="url(#paint0_linear_dark)"></path>
+      <defs>
+        <linearGradient id="paint0_linear_dark" x1="830" y1="83.5" x2="830" y2="338.5"
+          gradientUnits="userSpaceOnUse">
+          <stop stop-color="#08060B" stop-opacity="0.2"></stop>
+          <stop offset="0.545554" stop-color="#08060B" stop-opacity="0.5"></stop>
+          <stop offset="1" stop-color="#08060B"></stop>
+        </linearGradient>
+      </defs>
+    </svg>`;
+    }
     for (let i = 0; i < lightSwitches.length; i++) {
       lightSwitches[i].style = "transform: translateX(0px);";
       lightSwitches[i].querySelector("img").src =
@@ -150,23 +153,25 @@ function switchIt(evt) {
 
 function setFilterByEventListeners() {
   const activeButton = document.querySelector(".filter-button-switched.active");
-  const finishedButton = document.querySelector(
-    ".filter-button-switched.terminato"
-  );
-
-  activeButton.addEventListener("click", function () {
-    activeButton.classList.add("clicked");
-    activeButton.classList.remove("active");
-    finishedButton.classList.remove("clicked");
-    finishedButton.classList.add("active");
-  });
-
-  finishedButton.addEventListener("click", function () {
-    finishedButton.classList.add("clicked");
-    finishedButton.classList.remove("active");
-    activeButton.classList.remove("clicked");
-    activeButton.classList.add("active");
-  });
+  if(activeButton){
+    const finishedButton = document.querySelector(
+      ".filter-button-switched.terminato"
+    );
+  
+    activeButton.addEventListener("click", function () {
+      activeButton.classList.add("clicked");
+      activeButton.classList.remove("active");
+      finishedButton.classList.remove("clicked");
+      finishedButton.classList.add("active");
+    });
+  
+    finishedButton.addEventListener("click", function () {
+      finishedButton.classList.add("clicked");
+      finishedButton.classList.remove("active");
+      activeButton.classList.remove("clicked");
+      activeButton.classList.add("active");
+    });
+  }
 }
 //filter-by-switch-button
 
@@ -234,7 +239,7 @@ startSwiper();
 setLightSwitchesEventListeners();
 setDismissModalEventListeners();
 setBulletsEventListeners();
-//setFilterByEventListeners();
+setFilterByEventListeners();
 setSmallSwitchesEventListeners();
 setNormalSwitchesEventListeners();
 
@@ -243,9 +248,9 @@ let lastScrollTop; // This Varibale will store the top position
 let navbar = document.getElementById("navbar"); // Get The NavBar
 let antiphishing = document.getElementById("antiphishing-banner");
 // Hide the antiphishing banner by clicking the x icon
-let icon = document.getElementById("x-icon");
+let iconD = document.getElementById("x-icon");
 let hidden = false;
-icon.addEventListener("click", function () {
+iconD.addEventListener("click", function () {
   let banner = document.getElementById("antiphishing-banner");
   if (banner) {
     banner.style.display = "none";
